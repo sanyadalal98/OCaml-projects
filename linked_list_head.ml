20c97cffa97221a963a11e3497d18580587bd8a1
sanyadalal98 <51772592+sanyadalal98@users.noreply.github.com>
2019-12-25 22:23:23 +0530

    Update linked_list_head.ml

--- linked_list_head.ml
+++ linked_list_head.ml
@@ -1,3 +1,33 @@
+type 'a llist =
+  | Nil
+  | Cons of (float * 'a) * 'a lcell * 'a lcell
+and 'a lcell = ('a llist) ref
+;;
+
+(* Constructing singleton linked lists, i.e. a linked list with just one element in it *)
+let singleton (init: float * 'a): 'a lcell ref =
+  let l = ref (Cons (init, ref Nil, ref Nil)) in
+  let front = ref l in
+  front
+;;
+
+(* Some sample linked lists. *)
+let empty_list = ref (ref Nil)
+let one_element_list = singleton (3.0, "a")
+let two_element_list =
+  let second = ref Nil in
+  let first = ref (Cons ((2.3, "b"), ref Nil, second)) in
+  second := Cons ((3.3, "a"), first, ref Nil);
+  ref first
+;;
+
+(* Converting linked lists to regular OCaml lists. *)
+let rec list_of_lcell lcell =
+  match !lcell with
+  | Nil -> []
+  | Cons (d, _, next) -> d :: list_of_lcell next
+;;
+
 (* add head to doubly linked list *)
 let add_head x head =
   match !(!head) with
